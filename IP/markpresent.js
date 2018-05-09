96eb4fabb5187165e6ac5c92db14fb07c978cdcf
Rohitgupta2017 <32256164+Rohitgupta2017@users.noreply.github.com>
2018-05-09 18:24:53 +0530

    Add files via upload

---
+++ IP/markpresent.js
@@ -0,0 +1,8 @@
+﻿function setValue(){
+    var c = document.getElementById("setVal").value;
+   // document.getElementById("practice").innerHTML=a;
+
+    var a = document.createElement("td");
+    var b =document.createTextNode(c);
+    a.appendChild(b);
+    document.getElementById("abc").appendChild(a);}
